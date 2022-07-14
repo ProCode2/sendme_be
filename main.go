e58adca868506fd33243ad8e22655793f7d2fe6a
ProCode2 <iamprobablycoding@gmail.com>
2022-07-14 16:12:11 +0530

    tried basic websockets

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func main() {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		// Upgrade upgrades the HTTP server connection to the WebSocket protocol.
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Print("upgrade failed: ", err)
+			return
+		}
+		defer conn.Close()
+
+		conn.WriteMessage(websocket.TextMessage, []byte("voila"))
+
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			log.Println(string(msg))
+		}
+
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "websockets.html")
+	})
+	http.ListenAndServe(":8080", nil)
+}
